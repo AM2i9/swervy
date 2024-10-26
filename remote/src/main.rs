600429cb79a75e4f53e1759c77a245b399eae1b0
Patrick Brennan <brennanpatrick20@gmail.com>
2024-10-25 23:01:24 -0400

    Ditch pscontroller-rs lib for remote

--- remote/src/main.rs
+++ remote/src/main.rs
@@ -2,10 +2,8 @@
 #![no_main]
 
 use esp_backtrace as _;
-use esp_hal::{delay::Delay, gpio::{Io, Level, NoPin, Output}, prelude::*, spi::{master::Spi, SpiBitOrder, SpiMode}};
+use esp_hal::{delay::Delay, gpio::Io, prelude::*, spi::{master::Spi, SpiBitOrder, SpiMode}};
 use esp_println::println;
-use pscontroller_rs::{dualshock::ControlDS, Device, PlayStationPort};
-// use pscontroller_rs::PlayStationPort;
 
 extern crate alloc;
 use core::mem::MaybeUninit;
@@ -44,106 +42,38 @@ fn main() -> ! {
 
     let mut spi = Spi::new(
         peripherals.SPI2,
-        250.kHz(),
+        100.Hz(),
         SpiMode::Mode3
     )
     .with_pins(
-        io.pins.gpio4, // clk, blue
-        io.pins.gpio5, // mosi, orange
-        io.pins.gpio6, // miso, brown, needs pullup resistor
+        io.pins.gpio4, // clk, blue (purple)
+        io.pins.gpio5, // mosi, orange (green)
+        io.pins.gpio6, // miso, brown, needs pullup resistor (blue)
         io.pins.gpio7 // cs, yellow
     ).with_bit_order(
         SpiBitOrder::LSBFirst,
         SpiBitOrder::LSBFirst
     );
 
-    
-    // let mut cs = Output::new(io.pins.gpio7, Level::Low);
-    // let mut psp = PlayStationPort::new(spi, Some(Output::new(io.pins.gpio7, Level::Low)));
-    // psp.enable_pressure().unwrap();
     loop {
-        
-        let mut command = [0x01, 0x42, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
-        // psp.enable_pressure().unwrap();
-
-        // println!("c: {:?}", command);
-        {
-            let data = spi.transfer(&mut command);
-
-            println!("{:?}", data.unwrap());
-        }
-
-        // let controller = match psp.read_input(None) {
-        //     Err(_) => {
-        //         println!("\rError reading controller");
-        //         continue;
-        //     },
-        //     Ok(x) => x,
-        // };
-
-        // match controller {
-        //     Device::None => {
-        //         println!("Missing.");
-        //     },
-        //     Device::Mouse(x) => {
-        //         println!("Mouse: X:{:02}, Y{:02}, L:{}, R:{}",
-        //             x.x,
-        //             x.y,
-        //             x.buttons.left(),
-        //             x.buttons.right());
-        //     }
-        //     Device::Classic(x) => {
-        //         println!("Classic - Start? {0}, Square? {1}",
-        //             x.buttons.start(),
-        //             x.buttons.square());
-
-        //         if x.buttons.start() && x.buttons.select() {
-        //             psp.enable_pressure().unwrap();
-        //         }
-        //     },
-        //     Device::AnalogJoystick(x) => {
-        //         println!("Analog - Start? {0} - R:{1:02x},{2:02x}, L:{3:02x},{4:02x}",
-        //             x.buttons.start(),
-        //             x.rx,
-        //             x.ry,
-        //             x.lx,
-        //             x.ly);
-        //     },
-        //     Device::DualShock(x) => {
-        //         println!("DualShock - Start? {0} - R:{1:02x},{2:02x}, L:{3:02x},{4:02x}",
-        //             x.buttons.start(),
-        //             x.rx,
-        //             x.ry,
-        //             x.lx,
-        //             x.ly);
-        //     },
-        //     Device::DualShock2(x) => {
-        //         println!("DualShock2 - Start? {0} - R:{1:02x},{2:02x} - X Pressure:{3:02x}",
-        //             x.buttons.start(),
-        //             x.rx,
-        //             x.ry,
-        //             x.pressures[6]);
-        //     },
-        //     Device::JogCon(x) => {
-        //         println!("JogCon - Buttons: {0:08b}, Wheel: {1}", x.buttons.bits(), x.jog_position())
-        //     }
-        //     Device::NegCon(x) => {
-        //         println!("NegCon- Buttons: {0:08b}, Twist: {1}, I:  {2}", x.buttons.bits(), x.twist, x.switchi)
-        //     }
-        //     Device::GunCon(x) => {
-        //         println!("\rGunCon - Trigger: {0}, A: {3}, B:{4}, X:{1} Y:{2}                    ",
-        //             x.buttons.trigger(),
-        //             x.x(),
-        //             x.y(),
-        //             x.buttons.a(),
-        //             x.buttons.b())
-        //     },
-        //     Device::ConfigurationMode => {
-        //         println!("Somehow we got stuck where we shouldn't be");
-        //     },
-        //     _ => println!("Unimplemented"),
-        // }
 
         delay.delay(10.millis());
+
+        // For some reason the controller I have will not respond to configuration mode and
+        // programmatically setting the controller to analog mode
+        // will have to do manually for now
+
+        // Poll controller
+        let mut buffer = [0x01, 0x42, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
+        
+        if let Err(e) = spi.transfer(&mut buffer) {
+            log::error!("Error polling controller: {:?}", e);
+            continue;
+        }
+
+        if buffer[0] == 0xff && buffer[1] == 0x73 {
+            let state = &buffer[3..];
+            println!("{:?}", state);
+        }
     }
 }
